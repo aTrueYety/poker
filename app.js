223f3421c4501e819f87bce4a846ea88af56c3eb
Markus <markussmadsbakken@gmail.com>
2023-08-30 16:15:58 +0200

    ws

--- app.js
+++ app.js
@@ -8,13 +8,21 @@ const app = express();
 //websocket
 var expressWs = require('express-ws')(app);
 
+//session init
+const session = require('express-session');
+const e = require('express');
+var SQLiteStore = require('connect-sqlite3')(session);
 
-//ws init
-app.ws("/echo", function(ws, req) {
-    ws.on('message', function(msg) {
-        ws.send(msg);
-    });
-});
+app.use(session({
+    secret: "GenererTilfeldigSenere", //endre dette senere. Lagre secret som et nytt tilfelfig nummer hver gang server starter?
+    resave: false,
+    saveUninitialized: true,
+    secure: false,
+    store: new SQLiteStore({db : 'sessions.db', dir: __dirname + '/database'})
+}));
+
+
+app.use(express.json()) 
 
 //lagre ws i egen liste med brukere
 //ws.send sender til bruker
@@ -23,10 +31,40 @@ app.ws("/echo", function(ws, req) {
 //css
 app.use(express.static(__dirname + '/public'));
 
-//start
+
+//start server
 app.listen(port)
 console.log("server started: http://localhost:"+port)
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/html/index.html'));
+});
+
+let test = {};
+
+app.ws("/wsInit", function(ws, req) {
+    test[req.sessionID] = {};
+    test[req.sessionID]["ws"] = ws;
+
+    ws.on("close",() => { 
+        console.log("close");
+        delete test[req.sessionID];
+    });
+});
+let players = {}; //initierer brukere
+
+// navn:{username: navn, res: res, c: chips, status: status, action: action}
+
+function getNumPlayers(){
+    return Object.keys(players).length;
+}
+
+app.post('/wstest', function(req, res) {
+    test[req.sessionID]["ws"].send("5")
+    //console.log(req.session.ws);
+    res.send("ok");
+});
+
+app.get("/test", function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/html/test.html'));
 });
---
+++ public/js/test.js
@@ -0,0 +1,26 @@
+btn = document.getElementById("btn");
+btn.onclick = function () {
+    test();
+}
+
+var websocket = new WebSocket("ws://" + window.location.href.split("/")[2] + "/wsInit");
+
+websocket.onmessage = function (event) {
+    console.log(event.data);
+};
+
+function test() {
+    var xhp = new XMLHttpRequest();
+    xhp.responseType = 'text';
+    xhp.open("post","/wstest",true);
+    xhp.setRequestHeader("Content-Type", "application/json")
+    xhp.send();
+    xhp.timeout = 2000;
+    xhp.onload = () => {
+        console.log(xhp.response);
+    }   
+
+    xhp.ontimeout = (e) =>{
+        console.log("timeout, try again");
+    }
+}
