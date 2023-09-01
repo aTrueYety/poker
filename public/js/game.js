36aefdb65d1a06c4661c2e088fab9c684ef4762f
Markus <markussmadsbakken@gmail.com>
2023-09-01 17:44:57 +0200

    PIXI is no more.

--- public/js/game.js
+++ public/js/game.js
@@ -1,70 +1,21 @@
 let window_width = window.innerWidth;
 let window_heigth = window.innerHeight;
 
-//pixi init
-const app = new PIXI.Application({ background: '#cccccc ', resizeTo: document.getElementById("game"), antialias: true});
-app.ticker.maxFPS = 30;
+let testCards = []
 
-document.getElementById("game").appendChild(app.view);
+//test
+testCards.push(new Card("Spades", 1));
+testCards.push(new Card("Spades", 2));
+testCards.push(new Card("Hearts", 3));
+testCards.push(new Card("Clubs", 4));
+testCards.push(new Card("Diamonds", 5));
 
-//backgroundContainer
-const backgroundContainer = new PIXI.Container({antialias: true});
-app.stage.addChild(backgroundContainer);
+let board = document.getElementById("board"); //get board
 
-//playercontainer
-const playerContainer = new PIXI.Container ({antialias: true})
-app.stage.addChild(backgroundContainer);
 
-//graphics init
-const graphics = new PIXI.Graphics();
+board.innerHTML = "<img src='render/assets/board.jpeg'>";
 
-class GameRenderer{
-    constructor(width, height){
-        this.width = width;
-        this.height = height;
-
-        //sprites
-        let boardTexture = PIXI.Texture.from("/render/assets/board.jpeg"); 
-        this.board = new PIXI.Sprite(boardTexture);
-        this.board.width = this.width;
-        this.board.height = this.height;
-    }
-
-    render(){
-        backgroundContainer.removeChildren(); //removed old background
-
-        backgroundContainer.addChild(this.board);
-    }
-
-    resize(){
-        this.width = window.innerWidth;
-        this.height = window.innerHeight;
-
-        this.board.width = this.width;
-        this.board.height = this.height;
-    }
+for(let i = 1; i < 6; i++){ //get and render center cards
+    let card = document.getElementById("c"+i);
+    card.innerHTML = "<img src='render/assets/cards/"+testCards[i-1].suit+"/"+testCards[i-1].value+".jpg'>";
 }
-
-class Card{
-    constructor(suit, value){
-        this.card = new PIXI.Sprite(PIXI.Texture.from("/render/assets/cards/"+suit+"/"+value+".png"));
-        this.suit = suit;
-        this.value = value;
-    }
-}
-
-let gameRenderer = new GameRenderer(window_width, window_heigth);
-gameRenderer.render();
-
-//resize
-var resizeTimeout;
-window.addEventListener("resize",function(){
-    clearTimeout(resizeTimeout);
-    resizeTimeout = setTimeout(resizeHandler,100); //kaller resizehandler når 500ms har gått uten resize
-});
-
-function resizeHandler(){ //funksjon for å calle resize etter delay
-    gameRenderer.resize();
-};
-
-
