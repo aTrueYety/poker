4ce982f2c059f390313c4fd5c5bb91b553620734
Markus <markussmadsbakken@gmail.com>
2023-11-27 17:15:10 +0100

    moved images

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,6 +1,5 @@
 
 export default function User({ params }: { params: { userId: string } }) {
-    console.log(params.userId)
     return (
         <div className="flex items-center justify-center">
             <h1>{params.userId}</h1>
