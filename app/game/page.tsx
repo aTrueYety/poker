445ce0caf89383fc7b9731ea26d498394a5854d7
Markus <markussmadsbakken@gmail.com>
2023-11-27 23:17:49 +0100

    textinput og games page

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,8 +1,25 @@
-
+import { TextInput, Button } from "@/components/input"
 export default function Game() {
     return (
-        <div className="flex items-center justify-center">
-            <h1>Play</h1>
+        <div className="flex flex-col items-center justify-center mt-10">
+            <div className="flex flex-row items-center justify-center h-20 w-1/2">
+                <TextInput placeholder="Enter game id..." className=" w-9/12" />
+                <Button variant={"primary"} className="ml-2 w-3/12">Join</Button>
+            </div>
+
+            <div className="mt-8 mb-8">
+                <h1 className="text-2xl">OR</h1>
+            </div>
+
+            <div className="flex flex-row items-center justify-center h-20 w-1/2">
+                <Button variant={"primary"} className="w-full">Create New Game</Button>
+            </div>
+
+            <div className="mt-8 mb-8 flex flex-row">
+                <div className="p-10">
+                    ongoing games
+                </div>
+            </div>
         </div>
     )
 }
--- components/input.tsx
+++ components/input.tsx
@@ -1,12 +1,14 @@
 "use client";
 
+import { useRef } from "react";
+
 export const Button = ({ variant, children, className, onClick, ...rest }: { variant?: String, children?: React.ReactNode, className?: String, onClick?: () => void }) => {
     return (
         <button
             className={
                 variant == "primary"
-                    ? `px-7 py-2 w-full rounded-lg bg-neutral-700 hover:bg-blue-500 text-white ${className}`
-                    : `px-7 py-2 w-full rounded-lg bg-white border-2 hover:bg-blue-700 text-blue-500 border-blue-500 ${className}`
+                    ? `px-7 py-2 w-full h-full rounded-lg bg-neutral-700 hover:bg-blue-500 text-white ${className}`
+                    : `px-7 py-2 w-full h-full rounded-lg bg-white border-2 hover:bg-blue-700 text-blue-500 border-blue-500 ${className}`
             }
             {...rest}
             onClick={onClick}
@@ -14,6 +16,62 @@ export const Button = ({ variant, children, className, onClick, ...rest }: { var
             {children}
         </button>
     );
-
 }
+
+export const TextInput = ({ placeholder, value, onSubmit, onChange, className }: { placeholder?: string, value?: string, onSubmit?: (s: string) => void, onChange?: (s: string) => void, className?: string }) => {
+    const element = useRef<HTMLInputElement>(null);
+
+    //placeholder
+    let pl = "Placeholder...";
+    if (placeholder) {
+        pl = placeholder;
+    }
+
+    let alwaysAcceptValues = [
+        "Backspace",
+        "Delete",
+        "ArrowLeft",
+        "ArrowRight",
+        "ArrowUp",
+        "ArrowDown",
+        "Tab",
+    ];
+
+    //function to handle key down
+    function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>, acceptedValues?: string[]) {
+        if (acceptedValues) {
+            if (!acceptedValues.includes(e.key)) {
+                if (!alwaysAcceptValues.includes(e.key)) {
+                    e.preventDefault();
+                    //add animation?
+                }
+            }
+        }
+    }
+
+    //function to handle submit
+    function handleSubmit(text: string) {
+        element?.current?.blur();
+        onSubmit ? onSubmit(text) : {};
+    }
+
+    return (
+        <div className={`w-52 h-full flex flex-row bg-neutral-700 border border-neutral-800 rounded-lg p-1 ${className}`}>
+            <input
+                ref={element}
+                className={`w-full h-full bg-transparent outline-none px-3`}
+                placeholder={pl}
+                defaultValue={value}
+                onChange={(e) => {
+                    onChange ? onChange(e.target.value) : null;
+                }}
+                onKeyDown={(e) => {
+                    e.key === "Enter"
+                        ? handleSubmit(element.current ? element.current.value : "")
+                        : handleKeyDown(e);
+                }}
+            />
+        </div>
+    );
+};
 
