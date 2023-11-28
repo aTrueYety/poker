be6aa4d79543a052d83eadf7545ee7bcce19e0d0
Markus <markussmadsbakken@gmail.com>
2023-11-28 11:32:15 +0100

    update play page

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -3,8 +3,8 @@ export default function Game() {
     return (
         <div className="flex flex-col items-center justify-center mt-10">
             <div className="flex flex-row items-center justify-center h-20 w-1/2">
-                <TextInput placeholder="Enter game id..." className=" w-9/12" />
-                <Button variant={"primary"} className="ml-2 w-3/12">Join</Button>
+                <TextInput placeholder="Enter game id..." className=" w-10/12" />
+                <Button variant={"primary"} className="ml-2 w-2/12">Join</Button>
             </div>
 
             <div className="mt-8 mb-8">
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -93,7 +93,7 @@ function SidebarContent({ isOpen, setIsOpen }: { isOpen: boolean, setIsOpen: (is
                         <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
                     </svg>
                 }
-                    redirectTo="/game"
+                    redirectTo="/play"
                     onClick={() => {
                         setIsOpen(false);
                     }} />
